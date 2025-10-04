dd17c4a9ca7fdca24a31122c3f00d3962f04ae3d
Vinicius Souza Dias <62678753+vinicius-10@users.noreply.github.com>
2025-10-04 17:24:00 -0300

    if no blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,21 +3,57 @@
 @section('title','Sedatines')
 
 @section('contend')
-    <div class="left-side">
-        
+    <div>
+        <div class="left-side">
+        </div>
+
+        <div class="main">
+            <div>
+                <h1>Sedatines</h1> 
+            </div>
+            
+        </div>
+
+        <div class="right-side">
+        </div>
     </div>
 
-    <div class="main">
-        <h1>Sedatines</h1> 
+    <div>
+        <div class="left-side">
+        </div>
 
-        <ul>
-            <li><a href="myrron">Myrron Run</a></li>
-            <li><a href="demon">Demon Red</a></li>
-            <li><a href="shadow">Shadow</a></li>
-        </ul>
-    </div>
+        <div class="main">
+            <div>
+                <ul>
+                    <li><a href="myrron">Myrron Run</a></li>
+                    <li><a href="demon">Demon Red</a></li>
+                    <li><a href="shadow">Shadow</a></li>
+                </ul>
+            </div>
+            
+        </div>
 
-    <div class="right-side">
-        
+        <div class="right-side">
+        </div>
     </div>
+    
+    
+    <div>
+        <div class="left-side">
+        </div>
+
+        <div class="main">
+            <div>
+                É {{ $isDayOrNight }}
+                @if ( $isDayOrNight  == 'nigth')
+                tenha cuidado
+                @endif
+            </div>
+            
+        </div>
+
+        <div class="right-side">
+        </div>
+    </div>
+    
 @endsection
