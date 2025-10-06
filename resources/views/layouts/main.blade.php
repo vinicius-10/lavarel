fb26b6e3a2bb943c55e1b6da41ed037db9ce31c1
Vinicius Souza Dias <62678753+vinicius-10@users.noreply.github.com>
2025-10-06 08:50:45 -0300

    framework de icones

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -68,5 +68,7 @@
             <footer>
                 Vini Dias &copy; 2025
             </footer>
+            <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+            <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     </body>
 </html>
